bf04a2effc8594aa60d44cab09b31a1ca1ebddb1
Bob Dlamini <118727166+CodeHermez@users.noreply.github.com>
2025-08-05 20:03:50 +0200

    Time: 6 ms (41.07%), Space: 48.9 MB (58.93%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int NumOfUnplacedFruits(int[] fruits, int[] baskets) {
+        // Dictionary<int,int>map = new Dictionary<int,int>();
+        // int counter = 0;
+        // for(int i=0; i<fruits.Length; i++){
+        //     for(int j = 0; j<fruits.Length;j++)
+        //     {
+        //         if(fruits[i]<=baskets[j] && !map.ContainsKey(baskets[j]))
+        //         {
+        //             map.Add(baskets[j],fruits[i]);
+        //             fruits[i] = -1;
+        //             break;
+        //         }
+        //         else if(map.ContainsKey(baskets[j]) && map[baskets[j]]==fruits[i])
+        //             fruits[i] = -1;
+        //     }
+        // }
+        // Console.Write("{");
+        // for(int i=0; i<fruits.Length; i++){
+        //     if(fruits[i] != -1)
+        //         counter++;
+        //     Console.Write(fruits[i] + ", ");
+        // }
+        // Console.Write("}");
+
+        // return counter;
+        bool[]check = new bool[fruits.Length];
+        int counter = 0;
+        for(int i=0; i<fruits.Length;i++)
+            for(int j=0;j<fruits.Length;j++)
+                if(!check[j] && fruits[i]<=baskets[j])
+                {
+                    check[j]=true;
+                    fruits[i]=-1;
+                    break;
+                }
+        for(int i=0;i<fruits.Length;i++)
+            if(fruits[i]!=-1)
+                counter++;
+        return counter;
+    }
+}
