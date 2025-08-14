fc43c7d740b6f330e76a072c873378b76790c645
Bob Dlamini <118727166+CodeHermez@users.noreply.github.com>
2025-08-14 14:34:44 +0200

    Time: 7 ms (14.75%), Space: 18 MB (17.55%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
@@ -3,12 +3,10 @@ class Solution:
         
         list = []
         for x in range(len(num)-2):
-            print(num[x:x+3])
             if num[x:x+3][0]==num[x:x+3][1] and num[x:x+3][1] ==num[x:x+3][2]:
                     list.append(num[x:x+3])
         
         if not len(list)==0:
-            print(max(list))
             return max(list)
         else:
             return ''
