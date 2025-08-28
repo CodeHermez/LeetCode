1ba489417d1dc259c0aaf209cb4b7d5f44f4bafb
Bob Dlamini <118727166+CodeHermez@users.noreply.github.com>
2025-08-28 13:37:35 +0200

    Time: 21 ms (44%), Space: 12.4 MB (48%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def sortMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        size=len(grid)
+        def getDiag(rev=False,l=0,k=0):
+            q = l
+            t =k
+            diag = []
+            while q<size and t<size:
+                diag.append(grid[q][t])
+                q+=1
+                t+=1
+            
+            diag.sort(reverse=rev)
+            q = l
+            t =k
+
+            for x in diag:
+                grid[q][t]=x
+                q+=1
+                t+=1
+
+        for r in range(size):
+            getDiag(True,l=r)
+
+        for c in range(1,size):
+            getDiag(k=c)
+            
+        return grid
+                
