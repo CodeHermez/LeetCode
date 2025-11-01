b4fac9455632ea79b951a9615509bd63bc6acec4
Bob Dlamini <118727166+CodeHermez@users.noreply.github.com>
2025-11-01 17:52:36 +0200

    Time: 57 ms (94.94%), Space: 99.6 MB (54.43%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(nums, head) {
+    var i = 0, prev = null, hd = null, checker= new ListNode(0,head) 
+    var now = checker
+    nums=new Set(nums)
+
+    while(now.next!==null)
+    {
+        if(nums.has(now.next.val))
+            now.next= now.next.next
+        else
+            now = now.next
+    }
+
+    return checker.next
+
+    // if (head===null)
+    //     return head
+    // for(const it of nums)
+    // {
+    //     hd=head
+    //     prev=null
+    //     while(hd !== null)
+    //     {
+    //         if(prev===null && hd.val===it)
+    //         {
+    //             prev = hd
+    //             hd = hd.next
+    //             prev.next = null
+    //             head = hd
+    //             prev = null
+    //         }
+    //         else if(hd.val===it && hd.next===null)
+    //         {    prev.next = null
+    //             hd = hd.next
+    //         }
+    //         else if(hd.val===it)
+    //         {
+    //             prev.next = hd.next
+    //             hd.next=null
+    //             hd=prev.next
+    //         }
+    //         else
+    //         {
+    //             prev = hd
+    //             hd = hd.next
+    //         }
+    //     }
+    // }
+    // return head
+};
